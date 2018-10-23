e093efd792752611397993bd28ad2d7ff0dd5b74
Michael Moussa <michael.moussa@gmail.com>
2018-10-23 10:51:29 -0400

    Change route table association calculation
    Base on az_count * # subnets per AZ rather than total # of
    public/private cidr ranges. *Should* result in the same value in most
    cases, but this variation accounts for the edge case where az_count * #
    subnets is less than count of ranges.

--- main.tf
+++ main.tf
@@ -116,13 +116,13 @@ resource aws_route "private_routes" {
 }
 
 resource aws_route_table_association "public_route_association" {
-  count          = "${length(var.public_cidr_ranges)}"
+  count          = "${var.az_count * var.public_subnets_per_az}"
   subnet_id      = "${element(aws_subnet.public_subnet.*.id, count.index)}"
   route_table_id = "${aws_route_table.public_route_table.id}"
 }
 
 resource aws_route_table_association "private_route_association" {
-  count          = "${length(var.private_cidr_ranges)}"
+  count          = "${var.az_count * var.private_subnets_per_az}"
   subnet_id      = "${element(aws_subnet.private_subnet.*.id, count.index)}"
   route_table_id = "${element(aws_route_table.private_route_table.*.id, count.index)}"
 }
