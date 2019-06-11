5972644b389fec236ebfa03b1e5f4c7f38662af0
bohn002 <nath@nbohn.com>
2019-06-11 12:51:52 -0500

    adding lifecycle rule for better cleanup

--- main.tf
+++ main.tf
@@ -51,6 +51,10 @@ resource aws_vpc "vpc" {
   enable_dns_support   = "${var.enable_dns_support}"
 
   tags = "${merge(local.base_tags, map("Name", var.vpc_name), var.custom_tags)}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource aws_vpc_dhcp_options "dhcp_options" {
