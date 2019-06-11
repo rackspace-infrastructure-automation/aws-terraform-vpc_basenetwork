a5d7ae7bf028d7e1767612d06e49de00bcc5cf9b
bohn002 <nath@nbohn.com>
2019-06-11 11:03:28 -0500

    cidr math is hard

--- examples/custom_azs.tf
+++ examples/custom_azs.tf
@@ -10,8 +10,8 @@ module "vpc" {
   custom_azs = ["us-west-2a", "us-west-2b"]
 
   cidr_range             = "10.0.0.0/19"
-  public_cidr_ranges     = ["10.0.1.0/22", "10.0.3.0/22"]
+  public_cidr_ranges     = ["10.0.4.0/22", "10.0.8.0/22"]
   public_subnets_per_az  = 1
-  private_cidr_ranges    = ["10.0.2.0/22", "10.0.4.0/22"]
+  private_cidr_ranges    = ["10.0.12.0/22", "10.0.16.0/22"]
   private_subnets_per_az = 1
 }
--- tests/test2/main.tf
+++ tests/test2/main.tf
@@ -10,10 +10,10 @@ module "vpc" {
   custom_azs = ["us-west-2a", "us-west-2b"]
 
   cidr_range             = "10.0.0.0/19"
-  public_cidr_ranges     = ["10.0.1.0/22", "10.0.3.0/22", "10.0.5.0/22", "10.0.7.0/22"]
+  public_cidr_ranges     = ["10.0.4.0/22", "10.0.8.0/22", "10.0.12.0/22", "10.0.16.0/22"]
   public_subnets_per_az  = 2
   public_subnet_names    = ["Hello", "World"]
-  private_cidr_ranges    = ["10.0.2.0/22", "10.0.4.0/22", "10.0.6.0/22", "10.0.8.0/22"]
+  private_cidr_ranges    = ["10.0.20.0/22", "10.0.24.0/22", "10.0.28.0/22", "10.0.32.0/22"]
   private_subnets_per_az = 2
   private_subnet_names   = ["Foo", "Bar"]
 }
