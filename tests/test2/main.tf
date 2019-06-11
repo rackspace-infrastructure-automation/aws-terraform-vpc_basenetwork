37dff7195f13a8adeb6c95be975f3f9a455b1683
bohn002 <nath@nbohn.com>
2019-06-11 13:26:33 -0500

    matching CFn tests

--- tests/test2/main.tf
+++ tests/test2/main.tf
@@ -9,11 +9,11 @@ module "vpc" {
   vpc_name   = "Test2VPC"
   custom_azs = ["us-west-2a", "us-west-2b"]
 
-  cidr_range             = "10.0.0.0/19"
-  public_cidr_ranges     = ["10.0.4.0/22", "10.0.8.0/22", "10.0.12.0/22", "10.0.16.0/22"]
+  cidr_range             = "172.19.0.0/19"
+  public_cidr_ranges     = ["172.19.16.0/22", "172.19.20.0/22", "172.19.24.0/22", "172.19.28.0/22"]
   public_subnets_per_az  = 2
   public_subnet_names    = ["Hello", "World"]
-  private_cidr_ranges    = ["10.0.20.0/22", "10.0.24.0/22", "10.0.28.0/22", "10.0.32.0/22"]
+  private_cidr_ranges    = ["172.19.0.0/22", "172.19.4.0/22", "172.19.8.0/22", "172.19.12.0/22"]
   private_subnets_per_az = 2
   private_subnet_names   = ["Foo", "Bar"]
 }
